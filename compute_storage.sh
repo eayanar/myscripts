73e5f0c2fde8cbca8c447d764a291acf46bec169
eayanar <70071076+eayanar@users.noreply.github.com>
2020-11-24 22:02:17 +0530

    Update for recognition of space in filename using -print0 | xargs -0

--- compute_storage.sh
+++ compute_storage.sh
@@ -3,7 +3,7 @@ egrep "[0-9]+G" size_by_folder.txt
 egrep "[0-9]+G" size_by_folder.txt > sorter_folder
 egrep "[0-9]+M" size_by_folder.txt >> sorter_folder
 egrep "[0-9]+K" size_by_folder.txt >> sorter_folder
-find ../. -size +100000k | xargs du -sh | sort -n -r > size_by_file.txt
+find ../. -size +100000k -print0 | xargs -0 du -sh | sort -n -r > size_by_file.txt
 egrep "[0-9]+G" size_by_file.txt > sorted_file
 egrep "[0-9]+M" size_by_file.txt >> sorted_file
 egrep "[0-9]+K" size_by_file.txt >> sorted_file
